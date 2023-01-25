ab47b4f772715d7a1a82c233981f3cfd7a224815
ConanProZs <colinden09@gmail.com>
2023-01-25 15:58:35 +0100

    files and stufffffffff

---
+++ Spam-Lang/execute.py
@@ -0,0 +1,59 @@
+import pyautogui
+import time
+
+def execute_line(line, linecount):
+
+    args = list(line.split(":"))
+    args[-1] = args[-1].strip()
+    args[0] = args[0].replace(" ", "")
+    ##print(args)
+
+    #print
+    if args[0] == "print":
+        print(args[1])
+        return
+
+    #spam
+    if args[0] == "spam":
+        try:
+            msg = args[1]
+            n = args[2]
+
+            for i in range(0,int(n)):
+                pyautogui.typewrite(msg + '\n')
+                print(f"(spamming {args[1]}: {i}/{n})", end="\r")
+                time.sleep(0.01)
+        except IndexError:
+            return "No count, (line: " + str(linecount) + ")"
+        return
+
+    #test
+    if args[0] == "test":
+        print("--TEST--")
+        return
+    
+    #sleep
+    if args[0] == "sleep":
+        try:
+            args[1] = int(args[1]) / 1000
+            print(f"(sleeping for {args[1]} second(s))")
+            time.sleep(int(args[1]))
+        except:
+            return "Value invalid, (line: " + str(linecount) + ")"
+        return
+
+    #exit
+    if line.startswith("exit"):
+        return 0
+
+    #comment
+    if line.startswith("#"):
+        return
+
+
+    #notfound
+    line = line.replace(" ", "")
+    if line == "\n":
+        pass
+    else:
+        return "Function not found, (line: " + str(linecount) + ")"
